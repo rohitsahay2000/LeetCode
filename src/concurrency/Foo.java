6a52bfbfc595b579a7b71862cd834da65954728e
rsahay <rohitsahay@in.ibm.com>
2020-03-16 12:57:18 +0530

    Committing print in order concurrency problem.

--- src/concurrency/Foo.java
+++ src/concurrency/Foo.java
@@ -1,26 +1,73 @@
+/*
+ * Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+ 
+
+Example 1:
+
+Input: [1,2,3]
+Output: "firstsecondthird"
+Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
+Example 2:
+
+Input: [1,3,2]
+Output: "firstsecondthird"
+Explanation: The input [1,3,2] means thread A calls first(), thread B calls third(), and thread C calls second(). "firstsecondthird" is the correct output.
+ 
+
+Note:
+
+We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seems to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+ * 
+ * 
+ */
+
+
 package com.rohit.concurrency;
 
 class Foo {
+	
+	public Foo() {
 
-    public Foo() {
-        
-    }
+	}
 
-    public void first(Runnable printFirst) throws InterruptedException {
-        
-        // printFirst.run() outputs "first". Do not change or remove this line.
-        printFirst.run();
-    }
+	private volatile boolean isFirstPrinted;
+	private volatile boolean isSecondPrinted;
 
-    public void second(Runnable printSecond) throws InterruptedException {
-        
-        // printSecond.run() outputs "second". Do not change or remove this line.
-        printSecond.run();
-    }
+	public void first(Runnable printFirst) throws InterruptedException {
 
-    public void third(Runnable printThird) throws InterruptedException {
-        
-        // printThird.run() outputs "third". Do not change or remove this line.
-        printThird.run();
-    }
+		// printFirst.run() outputs "first". Do not change or remove this line.
+		printFirst.run();
+		isFirstPrinted = true;
+	}
+
+	public void second(Runnable printSecond) throws InterruptedException {
+
+		// printSecond.run() outputs "second". Do not change or remove this
+		// line.
+		while (!isFirstPrinted) {
+			;
+		}
+		printSecond.run();
+		isSecondPrinted = true;
+	}
+
+	public void third(Runnable printThird) throws InterruptedException {
+
+		// printThird.run() outputs "third". Do not change or remove this line.
+		while (!isSecondPrinted) {
+			;
+		}
+		printThird.run();
+
+	}
+
+	
 }
