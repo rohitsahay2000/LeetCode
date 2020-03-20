397dc5299ba873551aac19454da0e62223c87b27
rsahay <rohitsahay@in.ibm.com>
2020-03-20 13:47:51 +0530

    Commit reverse word in a string.

---
+++ src/string/ReverseWordsInString.java
@@ -0,0 +1,58 @@
+/*
+ * Given an input string, reverse the string word by word.
+
+ 
+
+Example 1:
+
+Input: "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: "  hello world!  "
+Output: "world! hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Note:
+
+A word is defined as a sequence of non-space characters.
+Input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+You need to reduce multiple spaces between two words to a single space in the reversed string.
+ */
+
+package com.rohit.extras;
+
+public class ReverseWordsInString {
+
+	public static void main(String[] args) {
+
+		System.out.println(new ReverseWordsInString().reverseWords("  hello world!  "));
+
+	}
+
+	private String reverseWords(String s) {
+		
+		String str = s.trim().replaceAll(" +", " ");
+		
+		String[] words = str.split(" ");
+
+		StringBuilder strBuilder = new StringBuilder();
+
+		for (int k = words.length-1; k >=0; k--) {
+
+			strBuilder.append(words[k].toString());
+			strBuilder.append(" ");
+		}
+
+		String finalResult = strBuilder.toString().trim();
+
+		return finalResult.trim();
+
+	}
+}
